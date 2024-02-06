99695afe066b4148e0545ccf885d70c7f2079742
MOHD ARIF <159111547+techneutr@users.noreply.github.com>
2024-02-06 15:06:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+// let arr=[1,2,3,4,[44,55,22],[10,12,44,55]]; //convet this nested array in single array
+// let arr2=[arr.length];
+// let i=0;
+// for(let ele in arr){
+//     if(ele.length>1){
+//         for(let inner in ele){
+//             arr2[i]=inner;
+//             i++;
+
+//         }
+//     }else{
+//         arr2[i]=ele;
+//         i++;
+//     }
+// }
+// for(let qq in arr2){
+//     console.log(qq);
+// }
+
+
+// // write a programe string is plaindrome or not
+// let s="abab";
+// let first=0;
+// let last=s.length-1;
+// let flag =true;
+// while(first<=last){
+//     if(s.charAt(first)===s.charAt(last)){
+//         first++;
+//         last--;
+//     }else{
+//         console.log("false");
+//         flag=false;
+//         break;
+//     }
+    
+    
+// }
+// if(flag)
+// console.log("true");   
+
+
+
+// // // "Hello I am abhsihek" reverse this string
+// let s="Hello I am abhsihek";
+// let reverse="";
+
+// for(let i=0;i<s.length;i++){
+//     reverse=s.charAt(i)+reverse;
+// }
+// console.log(reverse.toString());
+// console.log(s.toString());
+
+// console.log(s.length);
+
+
+// // // "Hello I am abhsihek" reverse this string
+// let s="Hello I am abhsihek";
+// let reverse="";
+
+// for(let el of s){
+//     reverse=el+reverse;
+// }
+// console.log(reverse.toString());
+// console.log(s.toString());
+
+// console.log(s.length);
+
+// // "Hello I am abhsihek" reverse this string
+// let a =2;
+// let b = 4;
+// let c =return (a+b)
+// console.log(c);
+
+
+
+var sum =(a,b)=>{
+    return a+b
+}
+console.log(sum(2,2));
+
+var i,n=100;
+for(i=1;i<=100;i++)
+{
+    console.log(i);
+}
+
+
+
+
